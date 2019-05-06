51c0c840943f7fc0d268494dcbc25c1765d9820f
Justin Clift <justin@postgresql.org>
2019-05-06 20:44:10 +1000

    Initial commit.  Writes LLVM IR bitcode to goir1.bc
    Pretty much the code from Felix Angell's blog post here:
    
      https://blog.felixangell.com/an-introduction-to-llvm-in-go
    
    Modified to write to a file for playing around with.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Go IR
+
+This is a just a simple repo playing around with directly
+generating LLVM IR from Go, just for learning purposes.
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/aykevl/go-llvm"
+	"log"
+	"os"
+)
+
+func main() {
+	// Create a LLVM IR builder
+	builder := llvm.NewBuilder()
+	mod := llvm.NewModule("my_module")
+
+	// Create a function called "main"
+	main := llvm.FunctionType(llvm.Int32Type(), []llvm.Type{}, false)
+	llvm.AddFunction(mod, "main", main)
+
+	// Create a basic block
+	block := llvm.AddBasicBlock(mod.NamedFunction("main"), "entry")
+
+	// Set the instruction insert point
+	builder.SetInsertPoint(block, block.FirstInstruction())
+
+	// int a = 32
+	a := builder.CreateAlloca(llvm.Int32Type(), "a")
+	builder.CreateStore(llvm.ConstInt(llvm.Int32Type(), 32, false), a)
+
+	// int b = 16
+	b := builder.CreateAlloca(llvm.Int32Type(), "b")
+	builder.CreateStore(llvm.ConstInt(llvm.Int32Type(), 16, false), b)
+
+	// a + b
+	aVal := builder.CreateLoad(a, "a_val")
+	bVal := builder.CreateLoad(b, "b_val")
+	result := builder.CreateAdd(aVal, bVal, "ab_value")
+
+	// Return
+	builder.CreateRet(result)
+
+	// Write out the IR as bitcode
+	outFile, err := os.Create("goir1.bc")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = llvm.WriteBitcodeToFile(mod, outFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
